6fecfc64e7fa94bad0b617b5210fb74bdd9ec967
theocdl <theo.claudel@edu.ece.fr>
2024-01-29 11:04:18 +0100

    fix: error Lint

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,26 +3,19 @@ import * as main from '../src/main'
 
 const runMock = jest.spyOn(main, 'run')
 
-let debugMock: jest.SpyInstance
 let errorMock: jest.SpyInstance
 let getInputMock: jest.SpyInstance
-let setFailedMock: jest.SpyInstance
-let setOutputMock: jest.SpyInstance
 
 describe('action', () => {
   beforeEach(() => {
     jest.clearAllMocks()
 
-    debugMock = jest.spyOn(core, 'debug').mockImplementation()
     errorMock = jest.spyOn(core, 'error').mockImplementation()
     getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
-    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
-    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()  
   })
   // a refaire en ajoutant "getInputMock = jest.spyOn(core, 'getInput').mockImplementation()""
 
   it('sets the time output', async () => {
-
     getInputMock.mockImplementation((name: string): string => {
       switch (name) {
         case 'ISSUE_NUMBER':
